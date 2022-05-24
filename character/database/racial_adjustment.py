f23162f53cab25c3a31e418b9a217a67b93ec8fb
Texdom65 <105843639+Texdom65@users.noreply.github.com>
2022-05-24 14:29:57 -0500

    Co-authored-by: engndeveloper@gmail.com <engndeveloper@gmail.com>

--- character/database/racial_adjustment.py
+++ character/database/racial_adjustment.py
@@ -3,29 +3,29 @@
 def get_racial_adjust(character):
      
     if character.race == "dwarf":
-        character.constitution = character.constitution + 1
-        character.charisma = character.charisma - 1
+        character.constitution += 1
+        character.charisma -= 1
         return character
 
     elif character.race == "elf":
-        character.constitution = character.constitution - 1
-        character.dexterity = character.dexterity +1
+        character.constitution -= 1
+        character.dexterity += 1
         return character
 
     elif character.race == "gnome":
-        character.intelligence = character.intelligence + 1
-        character.wisdom = character.wisdom - 1
+        character.intelligence += 1
+        character.wisdom -= 1
         return character
 
     elif character.race == "halfling":
-        character.dexterity = character.dexterity + 1
-        character.strength = character.strength - 1
+        character.dexterity += 1
+        character.strength -= 1
         return character
 
     elif character.race == "half-orc":
-        character.strength = character.strength + 1
-        character.constitution = character.constitution + 1
-        character.charisma = character.charisma - 1
+        character.strength += 1
+        character.constitution += 1
+        character.charisma -= 1
         return character
 
     else:
