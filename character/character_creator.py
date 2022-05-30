8e3710ff017586a907e00fdde47c6b695d7ea7ed
Texdom65 <105843639+Texdom65@users.noreply.github.com>
2022-05-29 23:42:53 -0500

    Co-authored-by: Josh Kunz <JoshuaKunz@users.noreply.github.com>

--- character/character_creator.py
+++ character/character_creator.py
@@ -142,7 +142,8 @@ def character_creator():
 
     
     print_green(str(character))
-
+    print()
+    print(character.get_rogue_skills())
     print()
     character = get_hp_score(character)
     character = get_hp_adjust(character)
